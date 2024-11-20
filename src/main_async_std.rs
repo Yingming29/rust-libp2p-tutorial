0c39a2bc96528e4bfdb000b1b446c2f472b44378
Yingming29 <https://github.com/Yingming29>
2024-11-20 14:12:02 +0000

    th example using tokio

--- src/main_async_std.rs
+++ src/main_async_std.rs
@@ -5,34 +5,33 @@ use std::error::Error;
 use std::time::Duration;
 use tracing_subscriber::EnvFilter;
 
-#[tokio::main]
+#[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Initialize log filter
+    // filter for the log
     tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
-
-    // Create a Swarm with transport and behaviour
+    // new a Swarm and build transport  
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_tokio() // Using Tokio runtime
+        .with_async_std()
         .with_tcp(
             libp2p::tcp::Config::default(),
             libp2p::tls::Config::new,
             libp2p::yamux::Config::default,
         )?
         .with_behaviour(|_| ping::Behaviour::default())?
-        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
-        .build();
-
-    // Listen on all interfaces with a random port
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely.
+        .build();   // the behavior connects the underlying transport and the higher layer network behaviour
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    // Dial the peer identified by the multi-address from command-line arguments
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
     if let Some(addr) = std::env::args().nth(1) {
         let remote: Multiaddr = addr.parse()?;
         swarm.dial(remote)?;
         println!("Dialed {addr}")
-    }
-
-    // Main event loop
+    }    
+    // The main loop of the program.
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
@@ -40,4 +39,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
             _ => {}
         }
     }
+    // Ok(())
 }
